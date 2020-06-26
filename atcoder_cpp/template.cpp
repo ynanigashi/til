ac0276679af14e370977eeab898a5eaf47fb69b0
ynanigashi <26033486+ynanigashi@users.noreply.github.com>
2020-06-26 09:10:16 +0900

    update template.cpp

--- atcoder_cpp/template.cpp
+++ atcoder_cpp/template.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
-using namespace std;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
 
 int main() {
 
