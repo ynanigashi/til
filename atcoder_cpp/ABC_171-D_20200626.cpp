6ec41687131d15e31269d0a923628d034791be05
ynanigashi <26033486+ynanigashi@users.noreply.github.com>
2020-06-26 09:38:13 +0900

    resolving ABC 171-D not check

---
+++ atcoder_cpp/ABC_171-D_20200626.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+int main() {
+    int N;
+    cin >> N;
+    //vector<int> A;
+    int MX = 100001;
+    vector<int> int_list(MX);
+    ll sum = 0;
+    rep(i, N){
+        int a;
+        cin >> a; 
+      //  A.at(i) = a;
+        int_list.at(a) += 1;
+        sum += a; 
+    }
+    int Q;
+    cin >> Q;
+    rep(i, Q){
+        int B, C;
+        cin >> B >> C;
+        sum += (C-B) * int_list.at(B);
+        cout << sum;
+        int_list.at(C) += int_list.at(B);
+        int_list.at(B) = 0;
+    }
+}
