4744387babfa9029d65d76bdb3efe89b70d4a77f
ynanigashi <26033486+ynanigashi@users.noreply.github.com>
2020-08-22 15:39:03 +0900

    class test ps1

---
+++ other/class_test_20200822.ps1
@@ -0,0 +1,31 @@
+﻿Class test{
+    [String] $hogehoge;
+
+    [Void] show_title([int] $case_no){
+        if($case_no -eq 1) {
+            Write-Host "";
+            Write-host "=================================";
+            Write-host "CASE1：直接代入した場合";
+            Write-host "=================================";
+            Write-Host "";
+        }
+        elseif ($case_no -eq 2) {
+            Write-Host "";
+            Write-host "=================================";
+            Write-host "CASE2：自動設定のメソッドを利用した場合";
+            Write-host "=================================";
+            Write-Host "";
+        } else {
+            throw "値が違う";
+        }
+    }
+}
+
+$test = [test]::new();
+$test.show_title(1);
+$test.hogehoge = "assignment directory";
+$test.hogehoge;
+
+$test.show_title(2);
+$test.set_hogehoge("assignment through method");
+$test.get_hogehoge();
