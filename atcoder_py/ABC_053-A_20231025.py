0694bdb9b62beca0986fe3d2356709a484a8d99a
ynanigashi <26033486+ynanigashi@users.noreply.github.com>
2023-10-27 08:08:56 +0900

    done abc054 a yesterday

--- atcoder_py/ABC_053-A_20231025.py
+++ atcoder_py/ABC_053-A_20231025.py
@@ -1,2 +1,11 @@
-rate = int(input())
-print('ABC' if rate < 1200 else 'ARC')
+# if card is 1 change 14
+Alice, Bob = map(lambda n : 14 if n == '1' else int(n) , input().split())
+
+Winner = 'Draw'
+if Alice > Bob :
+    Winner = 'Alice'
+
+if Alice < Bob:
+    Winner = 'Bob'
+
+print(Winner)
