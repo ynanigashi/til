580d1e1f66072183e2c2bcfa663ad02e9936fe88
ynanigashi <26033486+ynanigashi@users.noreply.github.com>
2023-12-30 19:14:38 +0900

    fix 087c

--- atcoder_py/ABC_087-C_20231230.py
+++ atcoder_py/ABC_087-C_20231230.py
@@ -1,13 +1,9 @@
 num_of_cells = int(input())
 upper_cells = list(map(int, input().split()))
 lower_cells = list(map(int, input().split()))
-candies = upper_cells[0] + lower_cells[-1]
+ans = []
 for i in range(num_of_cells):
-    upper_way = sum(upper_cells[i+1:])
-    lower_way = sum(lower_cells[i:]) - lower_cells[-1]
-    if upper_way > lower_way:
-        candies += upper_cells[i+1]
-    else:
-        candies += lower_way
-        break
-print(candies) 
+    upper_sum = sum(upper_cells[:i+1])
+    lower_sum = sum(lower_cells[i:])
+    ans.append(upper_sum+lower_sum)
+print(max(ans))
