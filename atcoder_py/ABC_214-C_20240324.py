93e70d010faca5838028e10c9c456c4657321a10
ynanigashi <26033486+ynanigashi@users.noreply.github.com>
2024-03-24 12:32:46 +0900

    modify 214C

--- atcoder_py/ABC_214-C_20240324.py
+++ atcoder_py/ABC_214-C_20240324.py
@@ -1,9 +1,9 @@
-sum_limit, product_limit = map(int, input().split())
-count = 0
-for i in range(101):
-    for j in range(101):
-        for k in range(101):
-            if (i + j + k <= sum_limit and 
-                i * j * k <= product_limit):
-                count += 1
-print(count)  
+num_of_people = int(input())
+waiting_times = list(map(int, input().split()))
+times = list(map(int, input().split()))
+for i in range(num_of_people*2):
+    times[(i+1)%num_of_people] = min(times[(i+1)%num_of_people], 
+                                          times[i%num_of_people] + waiting_times[i%num_of_people])
+
+for time in times:
+    print(time)
