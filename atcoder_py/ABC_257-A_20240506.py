d5b12f7c7ee93b6d4f07385bf97753877c51ba1d
ynanigashi <26033486+ynanigashi@users.noreply.github.com>
2024-05-06 00:30:32 +0900

    solved: ABC257 A, B;

---
+++ atcoder_py/ABC_257-A_20240506.py
@@ -0,0 +1,2 @@
+num, target = map(int, input().split())
+print(chr(64 + int(target // num)))
---
+++ atcoder_py/ABC_257-B_20240506.py
@@ -0,0 +1,13 @@
+n,k,q = map(int, input().split())
+a = list(map(int,input().split()))
+l= list(map(int,input().split()))
+
+for i in range(q):
+    if(a[l[i]-1] == n):
+        continue
+    elif(l[i]==k):
+        a[l[i]-1]+=1
+    elif(a[l[i]-1]+1<a[l[i]]):
+        a[l[i]-1]+=1
+
+print(*a)
